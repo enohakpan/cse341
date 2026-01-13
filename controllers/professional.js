72b4bef7c97032fe71ba2814a1af4c03fedc321b
enohakpan <dashdash59600@gmail.com>
2026-01-13 11:18:06 +0100

    changed to contacts

--- controllers/professional.js
+++ controllers/professional.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/mongo');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAllData = async (req, res, next) => {
-  const result = await mongodb.getDb().db('cse341').collection('professionals').find();
+  const result = await mongodb.getDb().db('cse341').collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -11,7 +11,7 @@ const getAllData = async (req, res, next) => {
 
 const getData = async (req, res) => {
   const id = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db('cse341').collection('professionals').find({ _id: id });
+  const result = await mongodb.getDb().db('cse341').collection('contacts').find({ _id: id });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
